4754bc8aab97ba49056c012373054874090c147e
tappitiajay <tappitiajay@gmail.com>
2024-11-07 19:35:37 +0530

    Add axios type for recipe API response to resolve TypeScript error

--- src/api/recipeApi.ts
+++ src/api/recipeApi.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+interface Recipe {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+}
+
+interface RecipeResponse {
+  meals: Recipe[];
+}
 const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php';
 
 export const fetchRecipesByIngredient = async (ingredient: string) => {
-  const response = await axios.get(`${BASE_URL}?i=${ingredient}`);
+  const response = await axios.get<RecipeResponse>(`${BASE_URL}?i=${ingredient}`);
   return response.data.meals;
 };
